c391346b57ba9e5ae6309fe9f9fc90bbed04e282
Martin Rowe <martin.rowe@assessandimprove.co.uk>
2014-09-14 21:36:35 +0100

    Added innuit

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass =  require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function() {
+gulp.src('app/assets/scss/style.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('public/css/style.min.css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/assets/scss/style.scss', ['public/css/style.css']);
+//	sass --watch scss/style.scss:css/style.min.css --style compressed
+//	gulp.watch('app/assets/scss/**/*.scss', ['css']);
+
+});
+
+gulp.task('default', ['watch']);
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,9 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <title>All Posts</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+   <!-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" /> -->
+        <link rel="stylesheet" type="text/css" href="css/style.min.css">
 </head>
 <body>
+    
+<div class=grid><!--
+    
+--><!--
+    
+ --><!--
+
+--><div class="grid__item  six-twelfths">
+        <p>One Twelfth</p>
+         <p class=demo-block-grey-white>The comments on the right hand side of the screen show design principles.</p>
+  </p>
+    </div><!--
+
+--><!--
+    
+--><div class="grid__item  five-twelfths">
+        <p>eleven Twelfth</p>
+        <div class="marginalia">
+        <p class="marginalia  desk-one-fifth">This page is an example of grid based sparse design.</p>
+      </div>
+        
+    </div><!--
+
+    --><!--
+    
+--><div class="grid__item  one-twelfth">
+        <p>eleven Twelfth</p>
+        <div class="marginalia">
+        <p class="marginalia  desk-one-fifth">This page is an example of grid based sparse design.</p>
+      </div>
+        
+    </div><!--
+
+
+--></div>
     <div class="container">
         <nav class="navbar navbar-default" role="navigation">
             <div class="navbar-header">
